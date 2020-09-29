33aa67f8edfe98986bd8286cb6295bd73e342f3f
fwcd <fwcdmail@gmail.com>
2020-09-29 22:30:20 +0200

    Add unit tests

--- Tests/UtilsTests/XCTestManifests.swift
+++ Tests/UtilsTests/XCTestManifests.swift
@@ -2,8 +2,19 @@ import XCTest
 
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(UtilsTests.allTests),
-    ]
+	return [
+		testCase(CircularArrayTests.allTests),
+		testCase(ComplexTests.allTests),
+		testCase(TokenIteratorTests.allTests),
+		testCase(StringUtilsTests.allTests),
+		testCase(MathUtilsTests.allTests),
+		testCase(BinaryHeapTests.allTests),
+		testCase(StablePriorityQueueTests.allTests),
+		testCase(Mat2Tests.allTests),
+		testCase(RationalTests.allTests),
+		testCase(NDArrayTests.allTests),
+		testCase(MatrixTests.allTests),
+		testCase(BiDictionaryTests.allTests)
+	]
 }
 #endif
