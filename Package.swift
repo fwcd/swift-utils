f182435fb2b6d259521168b851df6ecdcd9f6c85
fwcd <fwcdmail@gmail.com>
2021-02-17 20:32:59 +0100

    Add XMLCoder

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.0.0"),
         .package(name: "Socket", url: "https://github.com/Kitura/BlueSocket.git", .upToNextMinor(from: "1.0.0")),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0")
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0"),
+        .package(url: "https://github.com/MaxDesiatov/XMLCoder.git", from: "0.12.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
