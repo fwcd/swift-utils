6b0fefcc89511fd5e5d6c0afa111a3ca0b61cfae
fwcd <fwcd@fwcd.dev>
2024-01-07 16:14:09 +0000

    Add initializer for binding from projected value

--- Sources/Utils/Binding.swift
+++ Sources/Utils/Binding.swift
@@ -13,6 +13,12 @@ public struct Binding<Value> {
         self
     }
 
+    /// Creates a binding from another.
+    public init(projectedValue: Binding<Value>) {
+        _get = projectedValue._get
+        _set = projectedValue._set
+    }
+
     /// Creates a binding with the given getter and setter.
     public init(get: @escaping () -> Value, set: @escaping (Value) -> Void) {
         _get = get
