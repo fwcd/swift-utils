9ff8993b71bd339020803f08c38ed1ae23aa159f
fwcd <fwcd@fwcd.dev>
2024-01-07 15:52:03 +0000

    Add doc comment to Box

--- Sources/Utils/Box.swift
+++ Sources/Utils/Box.swift
@@ -1,3 +1,7 @@
+/// A simple property wrapper that stores a value on the heap.
+///
+/// Useful for turning a value type into a reference type without manually
+/// writing a class.
 @propertyWrapper
 public class Box<T> {
     public var wrappedValue: T
