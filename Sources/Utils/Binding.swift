ce5eb50081724e05b2828a70ef1bf7624dc03c77
fwcd <fwcd@fwcd.dev>
2024-01-07 15:51:59 +0000

    Add Binding

---
+++ Sources/Utils/Binding.swift
@@ -0,0 +1,16 @@
+/// A mutable, reference-like property wrapper that can read and write a value.
+@propertyWrapper
+public struct Binding<Value> {
+    private let _get: () -> Value
+    private let _set: (Value) -> Void
+
+    public var wrappedValue: Value {
+        get { _get() }
+        set { _set(newValue) }
+    }
+
+    public init(get: @escaping () -> Value, set: @escaping (Value) -> Void) {
+        _get = get
+        _set = set
+    }
+}
