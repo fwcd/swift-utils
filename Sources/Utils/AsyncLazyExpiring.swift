f91189839538c36513b5f7ce917acebd031ec36a
fwcd <fwcd@fwcd.dev>
2024-07-03 23:47:52 +0200

    Add AsyncLazyExpiring

---
+++ Sources/Utils/AsyncLazyExpiring.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+// Unfortunately Swift (as of 5.10) does not support async property wrappers.
+// For clarity, the API does not use autoclosure either.
+
+/// A value that periodically expires and gets re-queried
+/// through a supplied getter.
+public struct AsyncLazyExpiring<T> {
+    public let expiryInterval: TimeInterval
+    public private(set) var nextExpiry: Date!
+    private var expired: Bool { nextExpiry.timeIntervalSinceNow < 0 }
+
+    private let getter: () async throws -> T
+    private var cachedValue: T?
+    public var wrappedValue: T {
+        mutating get async throws {
+            if expired || cachedValue == nil {
+                try await update()
+            }
+            return cachedValue!
+        }
+    }
+
+    public init(in expiryInterval: TimeInterval = 1.0, wrappedValue getter: @escaping () async throws -> T) {
+        self.getter = getter
+        self.expiryInterval = expiryInterval
+    }
+
+    private mutating func update() async throws {
+        cachedValue = try await getter()
+        nextExpiry = Date(timeInterval: expiryInterval, since: Date())
+    }
+}
