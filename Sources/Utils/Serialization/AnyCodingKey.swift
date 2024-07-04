a89cdbced29abba2cec1327a32b3581e90075db9
fwcd <fwcd@fwcd.dev>
2024-07-04 21:44:23 +0200

    Conform AnyCodingKey to ExpressibleByLiteral protocols

--- Sources/Utils/Serialization/AnyCodingKey.swift
+++ Sources/Utils/Serialization/AnyCodingKey.swift
@@ -2,13 +2,25 @@ public struct AnyCodingKey: CodingKey {
     public let stringValue: String
     public let intValue: Int?
 
-    public init?(stringValue: String) {
+    public init(stringValue: String) {
         self.stringValue = stringValue
         intValue = nil
     }
 
-    public init?(intValue: Int) {
+    public init(intValue: Int) {
         self.intValue = intValue
         stringValue = "\(intValue)"
+    }
+}
+
+extension AnyCodingKey: ExpressibleByStringLiteral {
+    public init(stringLiteral value: String) {
+        self.init(stringValue: value)
+    }
+}
+
+extension AnyCodingKey: ExpressibleByIntegerLiteral {
+    public init(integerLiteral value: Int) {
+        self.init(intValue: value)
     }
 }
