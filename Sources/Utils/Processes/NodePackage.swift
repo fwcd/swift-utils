06329e9e75fd34cc8a326b36db58e6316c451d61
fwcd <fwcd@fwcd.dev>
2024-09-19 02:31:33 +0200

    Deprecate NodePackage

--- Sources/Utils/Processes/NodePackage.swift
+++ Sources/Utils/Processes/NodePackage.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-/// An executable node package that is located in the `Node` folder of this
-/// repository and is meant to be used in conjunction with this project, e.g. by
-/// exposing functionality from Node.js libraries.
+/// A wrapper around an executable node package that is located in `Node` under
+/// the current working directory.
+@available(*, deprecated, message: "This is too project-specific and will be removed from this library.")
 public struct NodePackage {
     private let directoryURL: URL
 
