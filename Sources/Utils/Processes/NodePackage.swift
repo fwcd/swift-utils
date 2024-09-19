cec3ab8f505586a60c595aa7ada781bca70c2557
fwcd <fwcd@fwcd.dev>
2024-09-19 02:27:48 +0200

    Reformat doc comments in NodePackage

--- Sources/Utils/Processes/NodePackage.swift
+++ Sources/Utils/Processes/NodePackage.swift
@@ -1,14 +1,8 @@
 import Foundation
 
-/**
- * An executable node package that
- * is located in the `Node` folder
- * of this repository and is meant
- * to be used in conjunction with
- * this project, e.g. by exposing
- * functionality from Node.js
- * libraries.
- */
+/// An executable node package that is located in the `Node` folder of this
+/// repository and is meant to be used in conjunction with this project, e.g. by
+/// exposing functionality from Node.js libraries.
 public struct NodePackage {
     private let directoryURL: URL
 
@@ -16,7 +10,7 @@ public struct NodePackage {
         directoryURL = URL(fileURLWithPath: "Node/\(name)")
     }
 
-    /** Invokes `npm start` with the given arguments. */
+    /// Invokes `npm start` with the given arguments.
     public func start(withArgs args: [String]) -> Promise<Data, Error> {
         Shell().output(for: "npm", in: directoryURL, args: ["run", "--silent", "start", "--"] + args)
     }
