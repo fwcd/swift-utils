a9e277fac675ba97f1540a11c409032f6f7ff19c
fwcd <fwcd@fwcd.dev>
2024-10-15 17:39:08 +0200

    Add async filter/map/flatMap to Optional

--- Sources/Utils/Extensions/Optional+Extensions.swift
+++ Sources/Utils/Extensions/Optional+Extensions.swift
@@ -2,4 +2,22 @@ public extension Optional {
     func filter(_ predicate: (Wrapped) throws -> Bool) rethrows -> Wrapped? {
         try flatMap { try predicate($0) ? $0 : nil }
     }
+
+    // MARK: Async combinators
+
+    func filter(_ predicate: (Wrapped) async throws -> Bool) async rethrows -> Wrapped? {
+        try await flatMap { try await predicate($0) ? $0 : nil }
+    }
+
+    func map<T>(_ transform: (Wrapped) async throws -> T) async rethrows -> T? {
+        try await flatMap(transform)
+    }
+
+    func flatMap<T>(_ transform: (Wrapped) async throws -> T?) async rethrows -> T? {
+        if let self {
+            try await transform(self)
+        } else {
+            nil
+        }
+    }
 }
