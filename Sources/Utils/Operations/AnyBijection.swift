ae057a67faba9dbee2f6c471495f16dd96b9dc06
fwcd <fwcd@fwcd.dev>
2024-10-19 21:13:00 +0200

    Fix sendability of AnyBijection

--- Sources/Utils/Operations/AnyBijection.swift
+++ Sources/Utils/Operations/AnyBijection.swift
@@ -1,15 +1,15 @@
 public struct AnyBijection<V>: Bijection {
-    private let applyImpl: (V) -> V
-    private let inverseApplyImpl: (V) -> V
+    private let applyImpl: @Sendable (V) -> V
+    private let inverseApplyImpl: @Sendable (V) -> V
 
-    public init(apply applyImpl: @escaping (V) -> V, inverseApply inverseApplyImpl: @escaping (V) -> V) {
+    public init(apply applyImpl: @Sendable @escaping (V) -> V, inverseApply inverseApplyImpl: @Sendable @escaping (V) -> V) {
         self.applyImpl = applyImpl
         self.inverseApplyImpl = inverseApplyImpl
     }
 
     public init<B>(_ bijection: B) where B: Bijection, B.Value == V {
-        applyImpl = bijection.apply
-        inverseApplyImpl = bijection.inverseApply
+        applyImpl = { bijection.apply($0) }
+        inverseApplyImpl = { bijection.inverseApply($0) }
     }
 
     public func apply(_ value: V) -> V { return applyImpl(value) }
